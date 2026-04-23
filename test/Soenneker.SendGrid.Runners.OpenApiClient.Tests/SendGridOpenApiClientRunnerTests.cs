86c07bd6e32a65aa1a4e54097e64f937680cfcd4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:45:54 -0500

    tunit

--- test/Soenneker.SendGrid.Runners.OpenApiClient.Tests/SendGridOpenApiClientRunnerTests.cs
+++ test/Soenneker.SendGrid.Runners.OpenApiClient.Tests/SendGridOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.SendGrid.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class SendGridOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class SendGridOpenApiClientRunnerTests : HostedUnitTest
 {
-    public SendGridOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public SendGridOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
